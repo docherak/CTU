44ba135ec38d5730eaba944ccc99f6fabae27176
docherak <docherak@protonmail.ch>
2022-05-28 00:18:42 +0200

    Add instructions

--- ZALG/Zapoctovy_Program/verze_2/main.cpp
+++ ZALG/Zapoctovy_Program/verze_2/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "Tridy.h"
 
-void pathfinder() {
+// OSETRIT KDYZ NEEXISTUJE START/CIL
+
+int main() {
 	std::cout << R"(
 		|				|
 		|	A* 	algoritmus	|
@@ -9,17 +11,39 @@ void pathfinder() {
 	)" << std::endl;
 	std::cout << R"(
 	Program nacita maticove bludiste ze spravne naformatovaneho textoveho souboru
-a nasledne vyuziva A* algoritmus pro co nejkratsi pruchod bludistem od oznaceneho
-pocatku az do cile.
+	a nasledne vyuziva A* algoritmus pro co nejkratsi pruchod bludistem od oznaceneho
+	pocatku az do cile.
+
+	Vstupni soubor s pevnym nazvem vstup.txt je nutne dat do stejne slozky jako program
+	a pro spravne fungovani musi byt obsah v nasledujicim formatu:
+	
+		m n				// prvni radek souboru
+		p[1,1]	.....	p[1,n]
+		.   .		.
+		.	.	.
+		.	    .	.	
+		p[m,1]	.....	p[m,n]		// posledni radek souboru
+
+		m - pocet radku v matici
+		n - pocet sloupcu v matici
+		p[i,j] - ij-te prvky matice, pro ktere plati:
+			- '.' znaci volny prostor
+			- 'X' znaci prekazku
+			- 's' znaci start
+			- 'c' znaci cil 
+	
+		napr.:
+		2 3
+		. . .
+		s X c
+
+	Program vykresli cely proces hledani cile (vsechny pozice agenta 'a') a nasledne
+	zobrazi pouze finalni optimalni trasu (oznacena pomoci 'O').
+
 	)" << std::endl;
-	std::cout << "Pro spusteni programu stisknete klavesu [Enter]..." << std::endl;
+	std::cout << "Pro nacteni vstupniho souboru stisknete klavesu [Enter]..." << std::endl;
 	std::cin.ignore();
-}
-
-int main() {
-
-	pathfinder();
-
+	
 	Matice *matice = nullptr;
 	
 	try {
@@ -36,9 +60,9 @@ int main() {
 		std::cin.ignore();
 	}
 
-	std::cout << "Pocet kroku: " << matice->ziskatNejkratsiCestu().size() << std::endl;
+//	matice->vypsatNejkratsiCestu(matice->ziskatNejkratsiCestu());
 	std::cout << "Cesta: " << std::endl;
-	matice->vypsatNejkratsiCestu(matice->ziskatNejkratsiCestu());
 	matice->vykreslitNejkratsiCestu(matice->ziskatNejkratsiCestu());
+	std::cout << "Pocet kroku: " << matice->ziskatNejkratsiCestu().size() << std::endl;
 	return 0;
 }
